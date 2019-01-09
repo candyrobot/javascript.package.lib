0229ecea604d7da6f0be19fb98adf19f730be7d2
田中 秀明 <tanaka_hideaki@aja-kk.co.jp>
2019-01-09 15:47:48 +0900

    update

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-import './object/Array';
-import './object/Math';
+import _Array from './object/Array';
+import _Math from './object/Math';
+import _Space from './object/Space';
+
+export const Array = _Array;
+export const Math = _Math;
+export const Space = _Space;
